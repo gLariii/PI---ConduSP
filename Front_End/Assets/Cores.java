6b3f11e9610577236e7b8223cf8676695ab364c5
LARISSA GOMES <25.00625-5@maua.br>
2025-05-13 12:21:19 -0300

    organização da tela da chave reversora

--- Front_End/Assets/Cores.java
+++
@@ -1,9 +0,0 @@
-package Assets;
-import java.awt.Color;
-
-public class Cores {
-
-    public static final Color AZUL_METRO = new Color(0, 20, 137); // Cor Azul Metrô - Castilho
-    public static final Color AZUL_METRO_TRANSPARENTE = new Color(0, 20, 137, 200); // Azul Metrô RGB - Castilho
-
-}
--- README.md
+++
@@ -1,8 +0,0 @@
-"""
-
- PI - Condução SP
- 
- O jogo Condução SP é um simulador interativo, point-click, para o treinamento de maquinistas do Metrô de São Paulo. O objetivo é capacitar 
- operários para lidar com falhas reais do sistema, como problemas em portas e maquinários. 
- 
-"""
