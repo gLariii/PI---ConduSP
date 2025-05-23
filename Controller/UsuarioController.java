e303f6187b0d6776b5dbcc5a77b8a9bed7421bf7
LARISSA GOMES <25.00625-5@maua.br>
2025-05-23 16:14:20 -0300

    Tratamento para tamanho do RG cadastrado

--- Controller/UsuarioController.java
+++ Controller/UsuarioController.java
@@ -7,13 +7,24 @@ import Model.Usuario;
 public class UsuarioController {
     private UsuarioDAO usuarioDAO = new UsuarioDAO();
 
+    private boolean RGValido(String rg) {
+        return rg != null && rg.matches("\\d{10}");
+    }
+    
     public boolean cadastrarUsuario(String rg, String senha, String nome, String tipoUsuario) {
+        if (!RGValido(rg)) {
+            System.out.println("RG inválido. Deve conter exatamente 10 dígitos numéricos.");
+            return false;
+        }
+    
         if (usuarioDAO.existeRg(rg)) {
             System.out.println("RG já cadastrado.");
             return false;
         }
-
+    
         Usuario novoUsuario = new Usuario(0, rg, senha, nome, tipoUsuario);
         return usuarioDAO.cadastrar(novoUsuario);
     }
+    
+    
 }
