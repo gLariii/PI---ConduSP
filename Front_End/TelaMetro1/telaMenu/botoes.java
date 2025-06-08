9b1f2d4456d576a6cf05af4163c26ca969aa5621
gLariii <larissa.souza@brazilglass.com.br>
2025-06-08 16:57:45 -0300

    fix: volume feat: pular introdução

--- Front_End/TelaMetro1/telaMenu/botoes.java
+++ Front_End/TelaMetro1/telaMenu/botoes.java
@@ -22,12 +22,12 @@ public class botoes {
     }
 
     public static JButton criarBotaoPadrao(String texto, String iconePath, int iconeWidth, 
-                                          int iconeHeight, int horizontalTextPosition, 
-                                          Dimension preferredSize, int fontSize) {
+                                            int iconeHeight, int horizontalTextPosition, 
+                                            Dimension preferredSize, int fontSize) {
         JButton button = new JButton(texto) {
             @Override
             protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
+                super.paintComponent(g); 
             }
         };
         
@@ -37,8 +37,7 @@ public class botoes {
         button.setOpaque(true);
         button.setBorder(BorderFactory.createCompoundBorder(
             BorderFactory.createLineBorder(new Color(255, 255, 255, 150), 2),
-            BorderFactory.createEmptyBorder(20, 40, 20, 20))) 
-         ;
+            BorderFactory.createEmptyBorder(20, 40, 20, 20)));
         button.setFocusPainted(false);
         button.setPreferredSize(preferredSize);
 
@@ -57,28 +56,30 @@ public class botoes {
 
     public static JButton criarBotaoMaquinario() {
         return criarBotaoPadrao("Maquinário", "/Assets/Imagens/metro.png", 80, 80,
-                               SwingConstants.RIGHT, new Dimension(500, 200), 40);
+                                 SwingConstants.RIGHT, new Dimension(500, 200), 40);
     }
 
     public static JButton criarBotaoFeedBackPessoal() {
         return criarBotaoPadrao("Feedbacks", "/Assets/Imagens/feedback.png", 80, 80,
-                               SwingConstants.RIGHT, new Dimension(500, 200), 40);
+                                 SwingConstants.RIGHT, new Dimension(500, 200), 40);
     }
 
     public static JButton criarBotaoSupervisor() {
         return criarBotaoPadrao("Supervisor", "/Assets/Imagens/supervisor.png", 80, 80,
-                               SwingConstants.RIGHT, new Dimension(500, 200), 40);
+                                 SwingConstants.RIGHT, new Dimension(500, 200), 40);
     }
     
     public static JButton criarBotaoDesconectar() {
         return criarBotaoPadrao("Desconectar", "/Assets/Imagens/sair.png", 50, 50,
-                               SwingConstants.LEFT, new Dimension(250, 70), 28);
+                                 SwingConstants.LEFT, new Dimension(250, 70), 28);
     }
 
     public static JButton criarBotaoConfiguracoes() {
         JButton button = new JButton() {
             @Override
             protected void paintComponent(Graphics g) {
+                super.paintComponent(g); 
+
                 Graphics2D g2d = (Graphics2D) g.create();
                 g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                 
@@ -97,19 +98,24 @@ public class botoes {
         }
         
         button.setPreferredSize(new Dimension(60, 60));
+        button.setOpaque(true); 
+        button.setBackground(Cores.AZUL_METRO); 
+        button.setBorderPainted(false); 
+        button.setContentAreaFilled(true); 
+
         return button;
     }
     
     public static JButton criarBotaoFechar() {
         return criarBotaoPadrao("Fechar", null, 0, 0,
-                              SwingConstants.CENTER, new Dimension(150, 50), 18);
+                                 SwingConstants.CENTER, new Dimension(150, 50), 18);
     }
 
 
     public static JButton criarBotaoVoltar() {
       
         JButton button = criarBotaoPadrao("", "/Assets/Imagens/seta.png", 40, 40,
-                               SwingConstants.CENTER, new Dimension(60, 60), 0);
+                                 SwingConstants.CENTER, new Dimension(60, 60), 0);
         
         button.setBorderPainted(false); 
         button.setContentAreaFilled(false); 
@@ -121,4 +127,4 @@ public class botoes {
         
         return button;
     }
-}
+}   
